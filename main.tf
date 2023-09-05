9ab3c51130b77b5a88a933355c3c628bc7fdfbb5
Jordan Fry <jordan.fry@coalfire.com>
2023-09-05 12:20:23 -0400

    Fixed formatting

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_kms_key" "kms_key" {
   description         = local.cmk_description
-  policy                  = var.key_policy
+  policy              = var.key_policy
   enable_key_rotation = true
 }
 
@@ -8,4 +8,3 @@ resource "aws_kms_alias" "kms_key_alias" {
   name          = "alias/${var.resource_prefix}-${var.kms_key_resource_type}"
   target_key_id = aws_kms_key.kms_key.key_id
 }
-
