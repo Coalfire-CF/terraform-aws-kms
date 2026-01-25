ea8ef6decd3e9669eb707479d386532492968831
Andy Mills <andy.mills@coalfire.com>
2026-01-25 18:29:37 -0500

    fix: merge vars to fix the un-used var.

--- locals.tf
+++ locals.tf
@@ -1,10 +1,8 @@
 locals {
   cmk_description = var.key_description != "" ? var.key_description : "${var.kms_key_resource_type} key for ${var.resource_prefix}"
 
-  internal_tags = {
-
+  internal_tags = merge(var.tags, {
     Managed-by = "Terraform"
     Module     = "terraform-aws-kms"
-  }
-
+  })
 }
